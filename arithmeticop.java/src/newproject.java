8446b8be8606aa043070756989b10e2e2054af08
Mayank1203 <mayankpatle1203@gmail.com>
2023-07-07 17:08:26 +0530

    first code

---
+++ arithmeticop.java/src/newproject.java
@@ -0,0 +1,40 @@
+ class newproject{
+    public static void main(String[] args){
+        //int num1=3;
+        //int num2=2;
+        //System.out.println(num1 + num2);
+        //System.out.println(num1 - num2);
+        //System.out.println(num1 * num2);
+        //System.out.println(num1 / num2);
+        //System.out.println(num1 % num2);
+
+        // INCREAMENT AND DECREAMENT OPERATOR
+
+        //int a=3;
+        //a++;//increament
+        //System.out.println(a);
+        //a--;//decreament
+        //System.out.println(a);
+
+//
+//        int a=5;
+//        int b=a++;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a=5;
+//        int b=++a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a=5;
+//        int b=a++ + ++a + --a;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a=5;
+//        int b=a++ + ++a + ++a + --a + a++ + a--;
+//        System.out.println(a);
+//        System.out.println(b);
+    }
+}
